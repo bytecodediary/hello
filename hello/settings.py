807b75e63d2a8d948827781bc8bb9ee5ecad8a43
Lawrence Wafula <72649310+trent130@users.noreply.github.com>
2024-10-06 08:07:59 +0000

    fixed the postgresql not functioning part and made the first migrations today

--- hello/settings.py
+++ hello/settings.py
@@ -48,12 +48,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.Middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -93,8 +93,8 @@ REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "realdb",
-        "PORT": "5433",
+        "NAME": "postgres",
+        "PORT": "5432",
         "USER": "postgres",
         "Host": "localhost",
         "PASSWORD": "larrymax",
